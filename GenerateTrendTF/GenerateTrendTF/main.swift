6fd2398b4e0593bad186799ef5eb96c32fc607a1
Eric4Bell <Eric4Bell@gmail.com>
2017-11-17 15:54:36 -0700

    Added simple neural network to predict whether there will be a trend over 30min time period in price of Bitcoin

---
+++ GenerateTrendTF/GenerateTrendTF/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GenerateTrendTF
+//
+//  Created by Eric Bell on 11/17/17.
+//  Copyright © 2017 Eric Bell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
